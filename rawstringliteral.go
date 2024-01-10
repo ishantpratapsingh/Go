4dee0a0604308607c229689f9bb197bd7d35856c
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-10 13:30:31 +0530

    Update rawstringliteral.go

--- rawstringliteral.go
+++ rawstringliteral.go
@@ -1,1 +1,11 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\t Hello, \n Ishant") // This is a regular string literal printed
+	fmt.Println(`This is a raw string literal \n`) //This is a raw string literal and so the string will be printed as it is and not the tab or new line
+}
 
