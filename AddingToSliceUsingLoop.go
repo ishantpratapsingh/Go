904b3ffd619bc64328dacf69e891c9395acd7ed3
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-17 12:12:17 +0530

    Create AddingToSliceUsingLoop.go

---
+++ AddingToSliceUsingLoop.go
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	sl1 := []int{}
+	for i := 0; i < 20; i++ {
+		sl1 = append(sl1, i)
+
+	}
+	fmt.Println(sl1)
+}
