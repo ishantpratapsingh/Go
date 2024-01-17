6091047630fc755b83741521c06fffb7124423d2
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-17 11:19:10 +0530

    Create NestedLoopBasic.go

---
+++ NestedLoopBasic.go
@@ -0,0 +1,18 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+
+		for j := 0; j < 10; j++ {
+
+			fmt.Println("This is from j", j)
+			fmt.Println("This is from i :", i)
+
+		}
+	}
+}
