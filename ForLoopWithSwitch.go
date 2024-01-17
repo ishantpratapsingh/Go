80b68a4f5daf0c964710b3b185e7ae2e371fcc5c
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-17 10:15:04 +0530

    Update ForLoopWithSwitch.go

--- ForLoopWithSwitch.go
+++ ForLoopWithSwitch.go
@@ -1,3 +1,23 @@
 // Write a program to print numbers from 0 to 100 and use switch case to run this loop 100 times
 
 
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 100; i++ {
+		fmt.Println("The number in this iteration is :  ", i)
+
+		switch {
+
+		case true:
+			fmt.Println("Number using switch is :  \t ", i)
+
+		}
+	}
+}
+
+
+
