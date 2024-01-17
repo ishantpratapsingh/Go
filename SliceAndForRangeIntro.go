1aa694a1704c70442b7f45eb58ba1d468a501482
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-17 11:57:15 +0530

    Create SliceAndForRangeIntro.go

---
+++ SliceAndForRangeIntro.go
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	sl1 := []int{1, 1, 9, 4, 2, 5}
+	for _, v := range sl1 {
+		fmt.Println(v)
+
+	}
+	fmt.Println(sl1)
+}
