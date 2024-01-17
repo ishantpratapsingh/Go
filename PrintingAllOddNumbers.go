c60f32773fba046a8a2635191e7c00c1bdaca544
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-17 11:16:51 +0530

    Update PrintingAllOddNumbers.go

--- PrintingAllOddNumbers.go
+++ PrintingAllOddNumbers.go
@@ -1,1 +1,19 @@
 
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 42; i++ {
+
+		switch {
+
+		case i%2 != 0:
+			fmt.Println("\n", i)
+
+		}
+	}
+}
