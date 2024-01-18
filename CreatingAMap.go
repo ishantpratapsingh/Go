ffd373fa4ac9d9fed193a49dbdf654031843c429
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-18 14:27:16 +0530

    Update CreatingAMap.go

--- CreatingAMap.go
+++ CreatingAMap.go
@@ -12,6 +12,15 @@ func main() {
 		"Bark":  22,
 		"Shark": 11}
 
-	fmt.Println(m)
+	fmt.Println(m) // This will print the entire map
 
+	/*This will not work if you have to print key and value in map. Only for range will work.
+	for i := 0; i < len(m); i++ {
+		fmt.Println("The key is : ", m[i])
+	} */
+
+	for i, v := range m {
+		fmt.Println("The value is :\t", v) //value of current iteration prints the value
+		fmt.Println("The key is :\t", i)   //Index of current iteration prints the key
+	}
 }
