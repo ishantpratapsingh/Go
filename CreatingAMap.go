c68b24546971bdc7b07817d88c7b708b11667f68
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2024-01-18 14:21:52 +0530

    Create CreatingAMap.go

---
+++ CreatingAMap.go
@@ -0,0 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{ //Declaring a map of Key (string) and value (int)
+		"Todd":  42,
+		"Mac":   55,
+		"Dark":  42,
+		"Bark":  22,
+		"Shark": 11}
+
+	fmt.Println(m)
+
+}
